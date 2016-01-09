50fb7a7f9effd82555abb6658c73ad4aa6a382ba
a-know <a.know.3373@gmail.com>
2016-01-09 20:14:02 +0900

    Enable to resize

--- app.rb
+++ app.rb
@@ -8,19 +8,19 @@ get '/' do
 end
 
 get '/graph/*' do |id|
-  width  = params[:width]  ? params[:width].to_i : 710
-  height = params[:height] ? params[:height].to_i : 110
   `curl https://github.com/#{id} | awk '/<svg/,/svg>/' | sed -e 's@<svg@<svg xmlns="http://www.w3.org/2000/svg"@' > ./tmp/grass.svg`
 
   svg_data = File.open('./tmp/grass.svg').read
-  png_data = ImageConvert.svg_to_png(svg_data, width, height)
+  png_data = ImageConvert.svg_to_png(svg_data, 710, 110)
 
-  if params[:rotate] && integer_string?(params[:rotate])
+  if params[:rotate] || params[:width] || params[:height]
+    width  = params[:width]  ? params[:width].to_i : 710
+    height = params[:height] ? params[:height].to_i : 110
+
     image = MiniMagick::Image.read(png_data)
     image.combine_options do |b|
-      # b.resize "250x200>"
-      b.rotate params[:rotate]
-      # b.flip
+      b.resize "#{width}x#{height}>" if params[:width] || params[:height]
+      b.rotate params[:rotate] if params[:rotate] && integer_string?(params[:rotate])
     end
     png_data = image.to_blob
   end
