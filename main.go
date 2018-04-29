1ea640be6351dfe411bcd4f0ecb0172b947be588
a-know <a.know.3373@gmail.com>
2018-04-29 15:37:26 +0900

    improve knock

--- main.go
+++ main.go
@@ -31,33 +31,40 @@ func main() {
 	plugins := &handlers.AssetsHandler{Kind: "plugins"}
 	r.Get("/plugins/*", plugins.HandleAssets)
 
-	r.Get("/knock", func(w http.ResponseWriter, r *http.Request) {
-		name := os.Getenv("SLACK_BOT_NAME")
-		text := "Visitor Incoming!!\nUA : hoge\nLanguage : fuga"
-		channel := os.Getenv("SLACK_CHANNEL_NAME")
+	r.Post("/knock", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		ua := r.Form.Get("user_agent")
+		lang := r.Form.Get("language")
+		admin := r.Form.Get("admin")
 
-		jsonStr := `{"channel":"` + channel + `","username":"` + name + `","text":"` + text + `"}`
+		if admin != "true" {
+			name := os.Getenv("SLACK_BOT_NAME")
+			text := fmt.Sprintf("Visitor Incoming!!\nUA : %s\nLanguage : %s", ua, lang)
+			channel := os.Getenv("SLACK_CHANNEL_NAME")
 
-		req, err := http.NewRequest(
-			"POST",
-			os.Getenv("SLACK_WEBHOOK_URL"),
-			bytes.NewBuffer([]byte(jsonStr)),
-		)
+			jsonStr := `{"channel":"` + channel + `","username":"` + name + `","text":"` + text + `"}`
 
-		if err != nil {
-			fmt.Print(err)
+			req, err := http.NewRequest(
+				"POST",
+				os.Getenv("SLACK_WEBHOOK_URL"),
+				bytes.NewBuffer([]byte(jsonStr)),
+			)
+
+			if err != nil {
+				fmt.Print(err)
+			}
+
+			req.Header.Set("Content-Type", "application/json")
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Print(err)
+			}
+
+			fmt.Print(resp)
+			defer resp.Body.Close()
 		}
-
-		req.Header.Set("Content-Type", "application/json")
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Print(err)
-		}
-
-		fmt.Print(resp)
-		defer resp.Body.Close()
 	})
 	http.ListenAndServe(":8080", r)
 }
